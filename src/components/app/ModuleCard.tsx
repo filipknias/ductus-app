f0a29568c5a4d02d4b25750c7ced36f6007ee0d4
Filip Kniaś <filipknias@MacBook-Air-Filip.local>
2025-02-09 14:30:52 +0100

    add ModuleCard tooltip

--- src/components/app/ModuleCard.tsx
+++ src/components/app/ModuleCard.tsx
@@ -10,9 +10,26 @@ type Props = {
 
 export default function ModuleCard({ stepIndicator, moduleName, status, moduleIcon }: Props) {
     return (
-        <button className="flex flex-col items-center cursor-pointer hover:-translate-y-4 transition duration-250">
+        <button className={clsx("flex flex-col items-center transition duration-250 relative group", status !== "locked" && "cursor-pointer hover:-translate-y-4")}>
+            {status === "locked" && (
+                <>
+                    <div className="absolute -top-16 left-1/2 transform -translate-x-1/2 hidden group-hover:block">
+                        <div className="bg-black/80 px-4 py-1 text-sm font-medium text-white whitespace-nowrap rounded-xl">Ukończ poprzedni poziom, aby odblokować ten!</div>
+                    </div>
+                    <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                        <div className="rounded-full bg-gray-500/70 flex items-center justify-center w-72 h-72">
+                            <img className="h-24" src="/lock-icon.png" alt="lock-icon" />
+                        </div>
+                    </div>
+                </>
+            )}
             <img className="h-36 mb-4" src={moduleIcon} alt={moduleName} />
-            <div className="bg-white/10 rounded-2xl py-1 px-4 shadow-md shadow-aqua-blue mb-4">
+            <div className={clsx(
+                    "bg-white/10 rounded-2xl py-1 px-4 shadow-md mb-4", 
+                    status === "in-progress" && "shadow-deep-blue", 
+                    status === "completed" && "shadow-lime-green",
+                    status === "locked" && "shadow-gray-500")}
+            >
                 <h3 className="font-medium text-lg text-white whitespace-nowrap">{stepIndicator}. {moduleName}</h3>
             </div>
             <div className={clsx(
--- src/components/views/DashboardView.tsx
+++ src/components/views/DashboardView.tsx
@@ -17,11 +17,11 @@ export default function DashboardView() {
             <div className="absolute top-1/2 right-0 transform -translate-y-1/2 w-160 h-160 bg-gradient-to-t from-blue-500 to-darkBlue-900 rounded-full filter blur-4xl"></div>
             <div className="absolute top-1/2 left-80 transform -translate-y-1/2 w-160 h-160 bg-gradient-to-t from-violet-500 to-darkBlue-900 rounded-full filter blur-4xl"></div>
             <div className="relative px-36 h-full flex flex-col justify-center">
-                <div className="flex flex-wrap -m-4 relative">
-                    <div className="w-full lg:w-7/12 p-4">
+                <div className="flex flex-wrap -m-12 relative">
+                    <div className="w-full lg:w-7/12 p-12">
                         <div className="flex flex-wrap -mx-4">
                             {modules.map((module) => (
-                                <div className="w-full lg:w-1/3 px-4 mb-12 flex justify-center" key={module.stepIndicator}>
+                                <div className="w-full lg:w-1/3 px-4 mb-24 flex justify-center" key={module.stepIndicator}>
                                     <ModuleCard 
                                         moduleIcon={module.moduleIcon}
                                         moduleName={module.moduleName}
@@ -32,7 +32,7 @@ export default function DashboardView() {
                             ))}
                         </div>
                     </div>
-                    <div className="w-full lg:w-5/12 p-4">
+                    <div className="w-full lg:w-5/12 p-12">
                         <ModuleContent moduleName={activeModule.moduleName}>
                             {activeModuleIndicator === 1 && <SchoolExperienceForm />}
                         </ModuleContent>
